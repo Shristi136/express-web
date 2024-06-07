3fba160e59dd40fe8a1dcff995d691ab6c364cba
Shristi Verma <shristiverma1306@gmail.com>
2024-06-07 15:40:56 +0530

    done with views and partials directary

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 const express =require ("express");
 const app = express();
 const path = require("path");
+const hbs = require("hbs");
 const port =process.env.PORT || 8000;
 
 const static_path = path.join(__dirname,"../Public");
-app.use(express.static(static_path));
+const template_path = path.join(__dirname,"../templates/views");
+const partial_path = path.join(__dirname,"../templates/partials");
 
-app.get("",(req,res)=>{
-    res.send("welcome to home page");
+app.set('view engine', 'hbs');
+app.set("views",template_path);
+hbs.registerPartials(partial_path);
+
+app.get("/",(req,res)=>{
+    res.render("index");
 })
 
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+
+app.get("/error",(req,res)=>{
+    res.render("error");
+})
+
+app.use(express.static(static_path));
 
 
 app.get("*",(req,res)=>{
